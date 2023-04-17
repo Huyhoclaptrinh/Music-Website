5c1013b4b956156c09b2df5e95ae48aee3cb5257
nghiahoang02 <hducnghia@gmail.com>
2023-04-17 18:10:54 +0700

    Update settings

--- Music Web Without Django/assets/js/style.js
+++ Music Web Without Django/assets/js/style.js
@@ -47,3 +47,26 @@ carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 carousel.addEventListener("mouseleave", dragStop);
 
+function openSettings(event, settingsName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(settingsName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+// Show default tab
+// document.getElementById("defaultOpen").click();
