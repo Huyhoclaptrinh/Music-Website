bdf7b2f72f21c14da3f979e16698a9e79f7d6e72
nghiahoang02 <hducnghia@gmail.com>
2023-05-11 14:59:05 +0700

    a sight of django

---
+++ Music Web Without Django/musiek/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def users(request):
+    template = loader.get_template('main_menu.html')
+    return HttpResponse(template.render())
