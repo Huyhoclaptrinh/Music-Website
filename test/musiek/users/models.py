edfcf801590659726230c79ff1a8fb22c9e61d31
nghiahoang02 <hducnghia@gmail.com>
2023-05-30 09:08:19 +0700

    context processors

--- test/musiek/users/models.py
+++ test/musiek/users/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Permission
 
 class UserRegister(AbstractUser):
     user_id = models.BigAutoField(
