0a45302cce1e51927d3a06716effb69ff25f3671
Huyhoclaptrinh <giahuyphung282@gmail.com>
2023-07-16 15:46:28 +0700

    Adding Sound Recognize System

--- test/musiek/musics/urls.py
+++ test/musiek/musics/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('library/<int:library_id>/delete/', views.delete_library, name='delete_library'),
     path('library/<int:library_id>/remove_song/<int:song_id>/', views.remove_song, name='remove_song'),
     path('save_to_history/<int:music_id>/', views.save_to_history, name='save_to_history'),
-    
+    path('sound_recognition/', views.sound_recognition, name='sound_recognition'),
+
 ]
